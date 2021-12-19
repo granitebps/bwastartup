a9476dc102725f98403c80211be126670bf20e3e
Granite Bagas <granitebagas28@gmail.com>
2021-12-19 21:04:24 +0700

    Add create campaign API

--- campaign/repository.go
+++ campaign/repository.go
@@ -8,6 +8,7 @@ type Repository interface {
 	FindAll() ([]Campaign, error)
 	FindByUserID(userID int) ([]Campaign, error)
 	FindByID(ID int) (Campaign, error)
+	Save(campaign Campaign) (Campaign, error)
 }
 
 type repository struct {
@@ -44,6 +45,15 @@ func (r *repository) FindByID(ID int) (Campaign, error) {
 	var campaign Campaign
 
 	err := r.db.Preload("CampaignImages").Preload("User").Where("id = ?", ID).Find(&campaign).Error
+	if err != nil {
+		return campaign, err
+	}
+
+	return campaign, nil
+}
+
+func (r *repository) Save(campaign Campaign) (Campaign, error) {
+	err := r.db.Create(&campaign).Error
 	if err != nil {
 		return campaign, err
 	}
--- handler/campaign.go
+++ handler/campaign.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/granitebps/bwastartup/campaign"
 	"github.com/granitebps/bwastartup/helper"
+	"github.com/granitebps/bwastartup/user"
 )
 
 type campaignHandler struct {
@@ -73,6 +74,46 @@ func (h *campaignHandler) GetCampaign(c *gin.Context) {
 		http.StatusOK,
 		"success",
 		campaign.FormatCampaignDetail(campaignDetail),
+	)
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *campaignHandler) CreateCampaign(c *gin.Context) {
+	var input campaign.CreateCampaignInput
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse(
+			"Failed to create campaign",
+			http.StatusUnprocessableEntity,
+			"error",
+			errorMessage,
+		)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+	input.User = currentUser
+
+	newCampaign, err := h.service.CreateCampaign(input)
+	if err != nil {
+		response := helper.APIResponse(
+			"Failed to create campaign",
+			http.StatusBadRequest,
+			"error",
+			nil,
+		)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse(
+		"Success to create campaign",
+		http.StatusOK,
+		"success",
+		campaign.FormatCampaign(newCampaign),
 	)
 	c.JSON(http.StatusOK, response)
 }
