bf7e47e40d9b5f37f37db2619564af98ef0ef63b
Granite Bagas <granitebagas28@gmail.com>
2021-12-22 21:13:15 +0700

    Add upload campaign image api

--- handler/campaign.go
+++ handler/campaign.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
@@ -167,6 +170,89 @@ func (h *campaignHandler) UpdatedCampaign(c *gin.Context) {
 		http.StatusOK,
 		"success",
 		campaign.FormatCampaign(updatedCampaign),
+	)
+	c.JSON(http.StatusOK, response)
+}
+
+func (h *campaignHandler) UploadImage(c *gin.Context) {
+	var input campaign.CreateCampaignImageInput
+	err := c.ShouldBind(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+		response := helper.APIResponse(
+			"Failed to upload campaign image",
+			http.StatusUnprocessableEntity,
+			"error",
+			errorMessage,
+		)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		data := gin.H{
+			"is_uploaded": false,
+		}
+		response := helper.APIResponse(
+			"Failed to upload campaign image",
+			http.StatusBadRequest,
+			"error",
+			data,
+		)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+	userID := int(currentUser.ID)
+	input.User = currentUser
+
+	// Create images folder if not exits
+	newpath := filepath.Join(".", "images")
+	os.MkdirAll(newpath, os.ModePerm)
+
+	path := fmt.Sprintf("images/%d-%s", userID, file.Filename)
+	err = c.SaveUploadedFile(file, path)
+	if err != nil {
+		data := gin.H{
+			"is_uploaded": false,
+		}
+		response := helper.APIResponse(
+			"Failed to upload campaign image",
+			http.StatusBadRequest,
+			"error",
+			data,
+		)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	_, err = h.service.SaveCampaignImage(input, path)
+	if err != nil {
+		data := gin.H{
+			"is_uploaded": false,
+		}
+		response := helper.APIResponse(
+			"Failed to upload campaign image",
+			http.StatusBadRequest,
+			"error",
+			data,
+		)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	data := gin.H{
+		"is_uploaded": true,
+	}
+	metaMessage := "Campaign Image successfully uploaded"
+	response := helper.APIResponse(
+		metaMessage,
+		http.StatusOK,
+		"success",
+		data,
 	)
 	c.JSON(http.StatusOK, response)
 }
